a507af4d8bb9ff89c6a12f4da79f18ae7003a1e6
KavinKily <gzlwhong101@gmail.com>
2019-09-03 16:47:02 +0800

    20190903链表

--- src/SingleLink/SingleLinkMain.java
+++ src/SingleLink/SingleLinkMain.java
@@ -4,11 +4,15 @@ package SingleLink;
 
 public class SingleLinkMain {
     public static void main(String[] args) {
+//        创建链表
         SingleLink singleLink = new SingleLink();
+//        添加结点
         singleLink.addLink("鲁班");
         singleLink.addLink("赵云");
         singleLink.addLink("韩信");
+//        输出链表
         singleLink.printLink();
+//        查找链表结点
         System.out.println(singleLink.findLinkNode("鲁班")+"cccc");
 
 
